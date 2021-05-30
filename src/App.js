01470d2b283ffdf08ca7fbeb6259b275009d6226
NirobNabil <thetwin11100@gmail.com>
2021-05-31 04:52:23 +0600

    kinda configured the base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import './styles/css/bootstrap.custom.min.css';
+import './styles/App.scss';
+// import $ from 'jquery';
+// import './js_inj/common_scripts'
+// import './js/main'
+// import './js/jquery.nice-select'
+// import './js/jquery.mmenu.all'
+// import './js/jquery.magnific-popup'
+// import './js/carousel-home'
+// import './js/jquery.cookiebar'
+// import './js/footer-reveal'
+// import './js/carousel_with_thumbs'
+// import './js/carousel-home-2'
+// import './js/bootstrap.bundle'
+// import './js/ResizeSensor'
+// import './js/wow'
+// import './js/video_header'
+// import './js/theia-sticky-sidebar'
+// import './js/specific_listing'
+// import './js/owl.carousel'
+// import './js/modernizr'
+// import './js/lazyload'
+
+import Home from './pages/home'
 
 function App() {
+  // const scripts = [
+
+  // ]
+  // useEffect( () => {
+    
+  // }, [] )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
